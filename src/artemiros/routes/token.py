a050bde0b3fde04e57159ba28c0e982c9e6cf881
Giovanni Chahin Morassi <giovannimorassi13511@gmail.com>
2025-10-16 15:12:59 -0300

    Envio do token

--- src/artemiros/routes/token.py
+++ src/artemiros/routes/token.py
@@ -1,12 +1,11 @@
-
-from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordRequestForm
-from schemas.token import TokenCreate, TokenData
-from settings import SETTINGS
 from database import AsyncSessionDep
+from fastapi import APIRouter
+from schemas.token import TokenCreate, TokenData
+from utils.aio_helpers import create_item
+
 router = APIRouter(prefix="/token", tags=["token"])
 
+
 @router.post("/")
-async def create_token(token: TokenCreate,session:AsyncSessionDep ) -> TokenData:
-    
-    
+async def create_token(token: TokenCreate, session: AsyncSessionDep) -> TokenData:
+    return await create_item(session=session, model=TokenData, data=token.model_dump())
